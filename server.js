31cb0321d1a7cb923ea2d6d71595f1d79b7015e8
Bruno Martínez <bmartinezg@minsait.com>
2020-04-24 18:45:17 +0200

    d

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const express = require('express');
 const app = express();
 // ROUTES
 app.use(require('./src/routes/index'))
+// PORT
+app.listen(process.env.PORT, () => {
+    console.log('escuchando en puerto 3000');
+})
