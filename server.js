f32249d6fbe7e0cefc7d55f2f5f2f9d6accbc57d
Bruno Martínez <bmartinezg@minsait.com>
2020-04-24 21:46:51 +0200

    static fix

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+// if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const express = require('express');
 const app = express();
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 // DB CONEXION
 mongoose.connect(process.env.DB , {useNewUrlParser: true, useUnifiedTopology: true});
-app.use(require('./src/routes/index'))
+// app.use(require('./src/routes/index'))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
@@ -17,6 +17,6 @@ app.use(express.static(__dirname + '/public/'))
 app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 // ROUTES
 // PORT
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('escuchando en puerto 3000');
 })
