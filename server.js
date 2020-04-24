f0e122c9bf25f423667c6cf2190d895e7b8d9005
Bruno Martínez <bmartinezg@minsait.com>
2020-04-24 18:27:27 +0200

    configuraciones de rutas

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const express = require('express');
 const app = express();
-
-app.listen(3000, () => {
+// ROUTES
+app.use(require('./src/routes/index'))
+// PORT
+app.listen(process.env.PORT, () => {
     console.log('escuchando en puerto 3000');
 })
-console.log(process.env);
