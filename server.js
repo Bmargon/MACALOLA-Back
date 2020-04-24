1f89cfa55425ade9c8d797a70231c9b5ed09d05e
Bruno Martínez <bmartinezg@minsait.com>
2020-04-24 21:50:35 +0200

    static fix final

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-
-// DB CONEXION
-//mongoose.connect(process.env.DB , {useNewUrlParser: true, useUnifiedTopology: true});
+// ROUTES
 app.use(require('./src/routes/index'))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,7 +13,8 @@ app.use(bodyParser.json())
 // FRONTEND
 app.use(express.static(__dirname + '/public/'))
 app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
-// ROUTES
+// DB CONEXION
+// mongoose.connect(process.env.DB , {useNewUrlParser: true, useUnifiedTopology: true});
 // PORT
 app.listen(process.env.PORT, () => {
     console.log('escuchando en puerto 3000');
