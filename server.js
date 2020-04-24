20e202a32225ea9486cfcc3e6036ddd296fc8987
Bruno Martínez <bmartinezg@minsait.com>
2020-04-24 18:09:01 +0200

    dependencias

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('hola');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('escuchando en puerto 3000');
+})
+console.log(process.env);
