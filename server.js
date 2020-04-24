4a24233ac61f7a4eb49ffc1581a1b1dffc694e6c
Bruno Martínez <bmartinezg@minsait.com>
2020-04-24 18:51:51 +0200

    integracion de body parser

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 // ROUTES
 app.use(require('./src/routes/index'))
 // PORT
