ac1d863c7dcb9614556010df4955880940d6a33e
Bruno Martínez <bmartinezg@minsait.com>
2020-04-26 12:31:47 +0200

    usuario administrador acceso

--- src/routes/user/login.js
+++ src/routes/user/login.js
@@ -58,9 +58,76 @@ app.post('/login', (req, res) => {
     res.status(200).json({
       success: true,
       token,
-      message: 'Acceso ok'
     })
   })
 })
+
+
+// ADMIN
+
+app.post('/login-admin', (req, res) => {
+
+  let body = req.body
+
+  User.findOne({email: body.email}, (err, userDB) => {
+
+    if (err) {
+      return res.status(502).json({
+        success: false,
+        err,
+        message: 'Ha ocurrido un error al intentar entrar'
+      })
+    }
+
+    if (!userDB) {
+      return res.status(502).json({
+        success: false,
+        err,
+        message: 'Este usuario no está registrado'
+      })
+    }
+
+    if (!userDB.active) {
+      return res.status(406).json({
+        success: false,
+        err,
+        message: 'Por motivos de seguridad su usuario ha sido desactivado'
+      })
+    }
+    
+    if (userDB.role !== 'ADMIN') {   
+      return res.status(406).json({
+        success: false,
+        message: 'No autorizado'
+      })
+    }
+
+    if (!bcrypt.compareSync(body.password, userDB.password)) {
+      
+      if (userDB.tries === 0 ){
+        userDB.active = false
+        userDB.save()
+        return res.status(406).json({
+          success: false,
+          message: 'Por motivos de seguridad su usuario ha sido desactivado'
+        })
+      }
+      userDB.tries -= 1;
+      userDB.save()
+
+      return res.status(502).json({
+        success: false,
+        message: 'Los campos son incorrectos'
+      })
+    }
+    let token = jwt.sign({user: userDB}, process.env.AUTHORIZATION, {expiresIn: '2h'})
+
+    res.status(200).json({
+      success: true,
+      token,
+    })
+  })
+})
+
 
 module.exports = app
