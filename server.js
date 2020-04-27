92ce82287466c05e8215e5f1b26045509f050076
Bruno Martínez <bmartinezg@minsait.com>
2020-04-27 17:00:30 +0200

    cambios

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 //app.use(sslRedirect())
-app.use(express.static(__dirname + '/public/'))
-app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 // parse application/json
 app.use(bodyParser.json())
 // parse application/x-www-form-urlencoded
@@ -15,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // ROUTES
 app.use(require('./src/routes/index'))
 // FRONTEND
+app.use(express.static(__dirname + '/public/'))
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 // DB CONEXION
 mongoose.connect(process.env.MONGODB_URI , {useNewUrlParser: true, useUnifiedTopology: true});
 // PORT
