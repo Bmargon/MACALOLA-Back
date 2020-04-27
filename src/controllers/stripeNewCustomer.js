45a2f48dc65cff4d8f16a6221be3d4086553d437
Bruno Martínez <bmartinezg@minsait.com>
2020-04-27 15:27:58 +0200

    usuario añadido a stripe al registrarse

--- src/controllers/stripeNewCustomer.js
+++ src/controllers/stripeNewCustomer.js
@@ -1,33 +1,33 @@
-const stripe = require('stripe')('pk_test_gsB61je2EKScjohgCX99U8nn00kwyWqPp1');
+const stripe = require('stripe')('sk_test_CR4G4g8akNZEGTOLhrizLhD500wCDzUOPt');
 
 const addStripeCostumer = async (user) => {
 
 
   try {
-    return await stripe.customers.create({
-      name: user.name + ' ' + user.username, // full name
-      shipping: {
-        name: user.name + ' ' + user.username,
-        phone: user.tlf,
-        address: {
-          line1: user.adressInvoicing.line1,
-          line2: user.adressInvoicing.line2,
-          city: user.adressInvoicing.city,
-          country: user.adressInvoicing.country,
-          postal_code: user.adressInvoicing.postal_code,
-          state: user.adressInvoicing.state
-        },
-      },
-      address: {
-        line1: user.adressInvoicing.line1,
-        line2: user.adressInvoicing.line2,
-        city: user.adressInvoicing.city,
-        country: user.adressInvoicing.country,
-        postal_code: user.adressInvoicing.postal_code,
-        state: user.adressInvoicing.state
-      },
+    return user =  await stripe.customers.create({
+      name: user.name, // full name
       email: user.email,
       phone: user.tlf,
+      // shipping: {
+      //   name: user.name ,
+      //   phone: user.tlf,
+      //   address: {
+      //     line1: user.adressInvoicing.line1,
+      //     line2: user.adressInvoicing.line2,
+      //     city: user.adressInvoicing.city,
+      //     country: user.adressInvoicing.country,
+      //     postal_code: user.adressInvoicing.postal_code,
+      //     state: user.adressInvoicing.state
+      //   },
+      // },
+      // address: {
+      //   line1: user.adressInvoicing.line1,
+      //   line2: user.adressInvoicing.line2,
+      //   city: user.adressInvoicing.city,
+      //   country: user.adressInvoicing.country,
+      //   postal_code: user.adressInvoicing.postal_code,
+      //   state: user.adressInvoicing.state
+      // }
     })
   } catch (error) {
     return new Error('No se pudo crear stripe user', error);
--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -7,53 +7,54 @@ const {addStripeCostumer} = require('../../controllers/stripeNewCustomer')
 // CREATE NEW USER
 app.post('/user', (req, res) => {
 
-    let body = req.body
-    
-    let user  = new User({
-        name: body.name,
-        username: body.username,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 4),
-        bdate: body.bdate,
-        genre: body.genre,
-        adress: body.adress,
-        tlf: body.tlf,
-        subscribe: body.subscribe,
-        role: body.role
+  let body = req.body
+  
+  let user  = new User({
+    name: body.name,
+    username: body.username,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 4),
+    bdate: body.bdate,
+    genre: body.genre,
+    adress: body.adress,
+    tlf: body.tlf,
+    subscribe: body.subscribe,
+    role: body.role
+  })
+
+  user.save((err, userDB) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        err,
+        message: 'Ha ocurrido un error al crear el usuario'
+      })
+    }
+    if (userDB.subscribe) {
+      addToSuscriptionList(userDB.email).catch((err) => {
+        res.status(503).json({
+          success: false,
+          err,
+          message: 'No se pudo añadir a la lista de suscripcin'
+        })
+      })
+    }
+    addStripeCostumer(userDB).then((user) => {
+      userDB.stripeID = user.id
+      userDB.save()
+    }).catch((err) => {
+      res.status(500).json({
+        success: false,
+        err,
+        message: 'El susuario no se pudo añadir a stripe'
+      })
     })
 
-    user.save((err, userDB) => {
-        if (err) {
-            return res.status(500).json({
-                success: false,
-                err,
-                message: 'Ha ocurrido un error al crear el usuario'
-            })
-        }
-        if (userDB.subscribe) {
-            addToSuscriptionList(userDB.email).catch((err) => {
-                res.status(503).json({
-                    success: false,
-                    err,
-                    message: 'No se pudo añadir a la lista de suscripcin'
-                })
-            })
-        }
-        addStripeCostumer(userDB).then((user) => {
-            console.log('usuario', userDB);
-            console.log('usuario añadido a stripe', user);
-        }).catch((err) => {
-            res.status(500).json({
-                success: false,
-                message: 'El susuario no se pudo añadir a stripe'
-            })
-        })
-
-        res.status(201).json({
-            success: true,
-            message: 'Usuario creado correctamente'
-        })
+    res.status(201).json({
+      success: true,
+      message: 'Usuario creado correctamente'
     })
+  })
 })
 
 module.exports = app
