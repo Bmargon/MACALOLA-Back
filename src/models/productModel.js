3ccd3ed8e48a612957b905865c9090acdfb4cdc5
Bruno Martínez <bmartinezg@minsait.com>
2020-05-04 21:40:27 +0200

    d

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -16,6 +16,7 @@ const Product = Schema({
   totalStock: {type: Number},
   accesory: {type: Boolean},
   img: {type: String, require: true},
+  imgs: {type: Array, require: false, default: []},
   cloudinary_id: {type: String},
   category: { type: Schema.Types.ObjectId, ref: 'Category'}
 })
